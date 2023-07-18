46499cb0f6fad13381c071b983b546820a374bc3
Orhan <kurtboganorhan@gmail.com>
2023-07-18 15:06:03 +0300

    'valid city' error fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 const APIUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-const APIKey = ""
+const APIKey = "e646d11a4a815b19ff39e2fbe05ba8b8"
 
    //divs
    const firstDiv = document.querySelector(".first-area")
@@ -18,93 +18,108 @@ const APIKey = ""
    const button = document.getElementById("button")
    const body = document.body
    const details = document.querySelector(".details")
-   const cities = document.querySelector(".city")
+   // const cities = document.querySelector(".city")
    const error = document.querySelector(".error")
+   const cityName = document.querySelector("h2")
    //
 
       app.style.display = "none"
 
 
-
+      
    const fetchAPI = async (city) => {
+      
       try {
-
-         const res = await fetch(APIUrl +  /*city*/encodedCity + `&appid=${APIKey}`)
-         const data = await res.json()
-
-         //DOC
-         const cityName = document.querySelector("h2").innerHTML = data.name
-         const temp = document.getElementById("temp").innerHTML = data.main.temp + ` °C`
-         const weather = document.getElementById("weath").innerHTML = data.weather[0].main
-         const min = document.getElementById("min").innerHTML =data.main.temp_min +  `°C`
-         const max = document.getElementById("max").innerHTML = data.main.temp_max + ` °C`
-         const wind = document.getElementById("wind").innerHTML = data.wind.speed + ` km/h`
-         const humidity = document.getElementById("humidity").innerHTML = data.main.humidity + `%`     
-         //DOC
-
-      
-
-
-         // DAY-  NIGHT
-         const img = document.getElementById("pic")
-         const currentTimeStamp = new Date()
-         const sunRise = new Date(data.sys.sunrise * 1000)
-         const sunSet = new Date(data.sys.sunset * 1000)
-      // DAY-  NIGHT
-      
-
+         const res = await fetch(APIUrl +  city + `&appid=${APIKey}`)
          
+          if(res.status == 404) {
+            error.setAttribute("style", "color:white; margin-top:26px")
+            error. style.display = "block"
+            details.style.display="none"
+            app.setAttribute("style", "height:200px")
 
-         switch(weather) {
-            case "Clear" : 
-            img.setAttribute("src", "js/img/sun.png")
-            break
 
-            case "Thunderstorm" :
-               img.setAttribute("src", "js/img/cloud-bolt-solid.svg")  
-               break
+         } else {
+            const data = await res.json()
 
-            case "Clouds":
-                  img.setAttribute("src", "js/img/cloudy.png")
-                  break
-            case "Mist":
-               img.setAttribute("src", "js/img/fog2.png")
-               break
-            case "Rain":
-               img.setAttribute("src", "js/img/rain.png")
-         }
-
-         if(currentTimeStamp > sunRise && currentTimeStamp < sunSet ) {
-            img
-         } else{
+            console.log("data")
+   
+            //DOC
+             const cityName = document.querySelector("h2").innerHTML = data.name
+             const temp = document.getElementById("temp").innerHTML = data.main.temp + ` °C`
+             const weather = document.getElementById("weath").innerHTML = data.weather[0].main
+             const min = document.getElementById("min").innerHTML =data.main.temp_min +  `°C`
+             const max = document.getElementById("max").innerHTML = data.main.temp_max + ` °C`
+             const wind = document.getElementById("wind").innerHTML = data.wind.speed + ` km/h`
+             const humidity = document.getElementById("humidity").innerHTML = data.main.humidity + `%`     
+            //DOC
+   
+         
+   
+   
+            // DAY-  NIGHT
+            const img = document.getElementById("pic")
+            const currentTimeStamp = new Date()
+            const sunRise = new Date(data.sys.sunrise * 1000)
+            const sunSet = new Date(data.sys.sunset * 1000)
+            // DAY-  NIGHT
+         
+   
+            
+   
             switch(weather) {
-               case "Clear":
-               img.setAttribute("src", "js/img/night-mode.png")
+               case "Clear" : 
+               img.setAttribute("src", "js/img/sun.png")
                break
-               case "Clouds":
-                  img.setAttribute("src", "js/img/cloud.png")
+
+               case "Thunderstorm" :
+                  img.setAttribute("src", "js/img/cloud-bolt-solid.svg")  
                   break
+   
+               case "Clouds":
+                     img.setAttribute("src", "js/img/cloudy.png")
+                     break
+
+               case "Mist":
+                  img.setAttribute("src", "js/img/fog2.png")
+                  break
+
                case "Rain":
                   img.setAttribute("src", "js/img/rain.png")
-            }
-         }
+                  break
 
+                  case "Drizzle":
+                     img.setAttribute("src", "drizzle.png")
+                     break
+            }
+   
+            if(currentTimeStamp > sunRise && currentTimeStamp < sunSet ) {
+               img
+            } else{
+               switch(weather) {
+                  case "Clear":
+                  img.setAttribute("src", "js/img/night-mode.png")
+                  break
+                  case "Clouds":
+                     img.setAttribute("src", "js/img/cloud.png")
+                     break
+                  case "Rain":
+                     img.setAttribute("src", "js/img/rain.png")
+               }
+            }
+            error. style.display = "none"
+            details.style.display="block"
+            app.removeAttribute("style")
+         }
          
 
       } catch(err) {
          console.log(err)
 
-         
-            if(cities.value === undefined) {
-            details.setAttribute("style", "margin-top: 60px; color: red; font-size: 20px")
-            details.innerHTML = "enter valid city name"; 
-         
 
-         } 
-   
-         
+         }      
 
-   } }
+   } 
 
    //first div eventlistener
 
@@ -125,17 +140,22 @@ const APIKey = ""
 
    //first div eventlistener
 
+
+
    body.addEventListener("keypress", e => {
       if(e.key === "Enter") {
          fetchAPI(text.value)
       }
    })
 
-   button.addEventListener("click", e => {
+   console.log(button.addEventListener("click", e => {
       fetchAPI(text.value);
       
-   }) 
+   }) ) 
+   
 
+   
+   
 
 
 
